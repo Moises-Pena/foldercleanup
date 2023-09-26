2c406864b434473d2a7a9dba8dd4142b7e5b5eaf
Moises-Pena <moises9798@gmail.com>
2023-09-26 16:41:52 -0400

    small mod

--- tomcatCleanup.bat
+++ tomcatCleanup.bat
@@ -3,17 +3,17 @@ setlocal enabledelayedexpansion
 
 set "targetDir=C:\Users\Moises\Desktop\tomcat"
 
-for /d %%i in ("%targetDir%\apache-tomcat-9.0.65*") do (
-    echo Deleting "%%i"
-    rmdir /s /q "%%i"
-)
-
-for /d %%i in ("%targetDir%\apache-tomcat-7*") do (
-    echo Deleting "%%i"
-    rmdir /s /q "%%i"
-)
-
 for /d %%i in ("%targetDir%\apache-tomcat-8*") do (
+    echo Deleting "%%i"
+    rmdir /s /q "%%i"
+)
+
+for /d %%i in ("%targetDir%\apache-tomcat-6*") do (
+    echo Deleting "%%i"
+    rmdir /s /q "%%i"
+)
+
+for /d %%i in ("%targetDir%\apache-tomcat-5*") do (
     echo Deleting "%%i"
     rmdir /s /q "%%i"
 )
